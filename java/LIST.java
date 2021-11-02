8055c0e29542c88c86d03ed015cef939fedbe92b
melchiottbakery <ysregister@outlook.com>
2021-11-02 20:36:11 +0000

    Update LIST.java
    cao

--- java/LIST.java
+++ java/LIST.java
@@ -3,18 +3,33 @@
 public class LIST {
 
     public int first;
-    public LIST second;
     public LIST rest;
+    public LIST ( int f, LIST r){
+        first = f;
+        rest =r;
+
+    }
+
+    public int get(int i){
+       if (i==0) {
+           return first;
+        }
+        return rest.get(i-1);
+    }
 
 
 
     public  static void main(String[] args) {
 
-        LIST L = new LIST();
-        L.first =5 ;
-        L.rest = null;
-        L.rest = new LIST();
-        L.rest.first=10;
+        LIST L = new LIST(15, null);
+        L = new LIST(10, L);
+        L = new LIST(5 ,L );
+
+        System.out.println(L.get(2));
+        //L.first =5 ;
+       // L.rest = null;
+       // L.rest = new LIST();
+       // L.rest.first=10;
 
        // LIST L = new LIST(5, null);
        // L.rest = new LIST(10, null);
